ed8ed577cc8702db175c22053591d75ff165ce40
Harsh Singh <68784731+HarshSingh-01@users.noreply.github.com>
2023-05-12 20:07:30 +0000

    adding some changes

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,13 @@
+import './ExpenseItem.css'
+
 const ExpenseItem = () => {
   return (
-    <div>
-      <p>Expense Item Component</p>
+    <div className="expense-item">
+      <div>May 12th 2023</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <p className="expense-item__price">$294.67</p>
+      </div>
     </div>
   );
 };
